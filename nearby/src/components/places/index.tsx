a0c12004e42350002800f62198b1ee85e90a24d7
DanielEgiidio <danielegidiot@gmail.com>
2025-01-21 11:50:22 -0300

    feat: Construção da pagina de detalhes do local - Finalização da aplicação

--- nearby/src/components/places/index.tsx
+++ nearby/src/components/places/index.tsx
@@ -26,7 +26,12 @@ export function Places({ data }: Props) {
       <BottomSheetFlatList
         data={data}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Place data={item} />}
+        renderItem={({ item }) => (
+          <Place
+            data={item}
+            onPress={() => router.navigate(`/market/${item.id}`)}
+          />
+        )}
         contentContainerStyle={s.content}
         ListHeaderComponent={() => (
           <Text style={s.title}>Explore locais perto de você</Text>
